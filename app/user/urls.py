e78e822cf51cc2a4d1450c6c721b56c1398f3bd4
terrificminds <suhail.ts@terrificminds.com>
2024-07-08 13:52:23 +0530

    user updations completed

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,6 +4,9 @@ URL mappings for the user API.
 from django.urls import path
 
 from user import views
+from decimal import Decimal
+
+from core import models
 
 
 app_name = 'user'
@@ -11,5 +14,7 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+
 
 ]
